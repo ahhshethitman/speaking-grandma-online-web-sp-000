93dcbd4a543175e315f95a41d19ce751273b90a9
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-31 17:41:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -23,4 +23,23 @@ def speak_to_grandma(speak)
     puts "NO, NOT SINCE 1938!"
   end
 end
+
+
+def speak_to_grandma(phrase)
+
+    if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+   
+    elsif phrase == phrase.downcase
+  		return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.downcase
+      return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
+   
+    else
+  		return "NO, NOT SINCE 1938!"
+    end
+
 end
+
