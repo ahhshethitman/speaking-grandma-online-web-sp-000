b4cd70b03a656ad45832be26ed311ea062ac2969
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-31 17:52:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
  
- else if phrase == phrase.downcase
+ elsif phrase == phrase.downcase
   return "HUH?! SPEAK UP, SONNY!"
   
  else
